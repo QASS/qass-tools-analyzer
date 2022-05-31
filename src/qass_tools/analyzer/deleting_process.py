fda262d1f6e1b161f6e09fe9e1a992c203dc9e8a
Opti Mizer <opti@opti000000.qass.office>
2022-05-31 15:57:17 +0200

    Add docstrings and implement exceptions

--- src/qass_tools/analyzer/deleting_process.py
+++ src/qass_tools/analyzer/deleting_process.py
@@ -4,37 +4,55 @@ import os
 import glob
 from typing import List
 import shutil
+from Analyzer.Core import Log_IF
 
 __all__ = ["DeleteHandler"]
 
 class DeleteHandler():
-    """ DeleteHandler Class with different functionalities to delete local files on disk. With an instance of this class a specific deleting-pattern
-    and path (for the files) will be associated. The programm always looks for the oldest versions of files which match with the pattern.
-    Files will be deletet until the maximum amount of allowed files is reached or enough disk space is free. This class stands for now on its own.
-    With a def main syntacs it will be executed by programm start."""
-    def __init__(self, path, pattern) -> None:
-        """ Each instance of this class connects a specific local path with a specific deleting pattern. Provided functions can be applied later for each combination.
-        Pattern has to be according to the rules used by Unix shell (used glob module is based on that). In the following Unix Shell rules are provided:
+    """DeleteHandler Class with different functionalities to delete local files on disk. 
+    
+    With an instance of this class a specific deleting-pattern and path (for the files) will be associated. The programm always looks for the oldest versions of files which match with the pattern.
+    Files will be deletet until the maximum amount of allowed files is reached or enough disk space is free. This class stands for now on its own. With a def main() syntacs it will be executed by programm start.
+    
+    :param path: Local directory path.
+    :type path: str
+    :param pattern: Searched pattern.
+    :type pattern: str
+    """
+    def __init__(self, path: str, pattern: str) -> None:
+        """Constructor to connect a specific local directory path with a specific pattern parsed.
+
+        Provided functions can be applied later for each combination. Pattern has to be according to the rules used by Unix shell (used glob module is based on that). In the following Unix Shell rules are provided:
+        | Pattern | Meaning                               | Example                                                                              |
+        | ------- | ------------------------------------- | ------------------------------------------------------------------------------------ |
+        | *	      | Matches everything	                  | *.pdf matches all files with the pdf extension                                       |
+        | ?	      | Matches any single character	      | sales/??.jpeg matches all files with two characters long present in the sales folder |
+        | []	  | Matches any character in the sequence |	[psr]* matches all files starting with the letter p, s, or r                         |
+        | [!]	  | Matches any character not in sequence |	[!psr]* matches all files not starting with the letter p, s, or r                    |
         
-        Pattern     Meaning                                     Example
-        *	        Matches everything	                        *.pdf matches all files with the pdf extension
-        ?	        Matches any single character	            sales/??.jpeg matches all files with two characters long present in the sales folder
-        []	        Matches any character in the sequence	    [psr]* matches all files starting with the letter p, s, or r
-        [!]	        Matches any character not in sequence	    [!psr]* matches all files not starting with the letter p, s, or r
-        For a literal match, wrap the meta-characters in brackets. For example, '[?]' matches the character '?'
+        For a literal match, wrap the meta-characters in brackets. For example, '[?]' matches the character '?'    
+
+        :param path: Local directory path.
+        :type path: str
+        :param pattern: Searched pattern.
+        :type pattern: str
         """
         self.path = path  
         self.pattern = pattern
         
-    def delete_by_amount(self, max_amount: int):
-        """ Provided function to delete files if file amount in opath location overruns defined limit. Limit is given as an integer in function parameters.
-        Exception is raised if deleting process cannot be compelted. Either because there are not enough files that match pattern to satisfy 
-        limit or because any other wild error appears """
+    def delete_by_amount(self, max_amount: int) -> None:
+        """Provided function to delete files based on file amount in location if amount overruns defined limit.
+
+        :param max_amount: Maximum amount (limit) of files allowed in thisn directory which are match pattern.
+        :type max_amount: int
+        :raise OSError: Exception is raised if deleting process cannot be compelted. Either because there are not enough files that match pattern to satisfy limit or because any other wild error appears. 
+        """
         # read out amount of files in directory
         dir_amount = len(os.listdir(self.path))
         # calc how many files have to be deletet in order to satisfy limit
         amount_to_delete = dir_amount - max_amount
         # if there are any files to delete:
+        
         if amount_to_delete > 0:
             # search for files in directory which match pattern
             delete_list = glob.glob(self.path + self.pattern) # delete_list is saved with full path
@@ -44,14 +62,19 @@ class DeleteHandler():
             try:
                 for idx in range(0, amount_to_delete):
                     self.__delete_file(sorted_list[idx][1])
-            except:
-                # raise exception
-                print("Deleting process could not be completed. Check your set Limit and your Files.")
-
+                print("Deleting successfull")
+            except OSError as e:
+                Log_IF().error(f"Deleting process could not be completed. Check your setted Limit and your Files.")
+                print(e)
+                print("Deleting process could not be completed. Check your setted Limit and your Files.")
             
-    def delete_by_disk_space(self, disk_usage_limit: float):
-        """ Provided function to delete by disk space. As parameter the maximum disk usage as part of a whole is entered as float. Function has no own 
-        exception. If a error occurs its root is in actual deleting function."""
+    def delete_by_disk_space(self, disk_usage_limit: float) ->None:
+        """Provided method to delete by disk space.
+
+        :param disk_usage_limit: Limit how much disk memory is allowed to use at maximum. Parsed as part of the whole.
+        :type disk_usage_limit: float
+        :raise OSError: Exception is raised if deleting process cannot be compelted. Either because there are not enough files that match pattern to satisfy limit or because any other wild error appears. 
+        """
         # read out currently used disk usage
         disk_usage = shutil.disk_usage(self.path)
         # help to debug: disk_usage[0] = total // disk_usage[1] = used // disk_usage[2] = free
@@ -72,13 +95,23 @@ class DeleteHandler():
                 filesize = filesize + os.path.getsize(file)
                 if filesize >= space_to_make:
                     break
-            for idx in range(0, n+1):
-                self.__delete_file(sorted_list[idx][1])
-
+            try:
+                for idx in range(0, n+1):
+                    self.__delete_file(sorted_list[idx][1])
+                    print("Deleting successfull")
+            except OSError as e:
+                Log_IF().error(f"Deleting process could not be completed. Check your setted Limit and your Files.")
+                print(e)
+                print("Deleting process could not be completed. Check your setted Limit and your Files.")
 
     def __get_oldest(self, possible_files: List)   -> List:
-        """ Private function to get a list of local files which are sorted by creation date. As possible files everythin is parsed which match
-        deleting pattern. As return it gives a list a sorted list with the oldest files as first entry."""
+        """Private method to get a list of local files which are sorted by creation date.
+
+        :param possible_files: Everything in directory which matches pattern.
+        :type possible_files: List
+        :return: Sorted list with the oldest files as first entry.
+        :rtype: List
+        """
         # helper
         creation_dates = []
         for file in possible_files:
@@ -95,11 +128,17 @@ class DeleteHandler():
     
         return sorted_ziped_lists
     
-    def __delete_file(self, deleting_file: str):
-        """ Private function to delete given files. An error is raisen if files cannot be removed or if parsed string is empty."""
+    def __delete_file(self, deleting_file: str) -> None:
+        """Private method to delete parsed file. 
+
+        :param deleting_file: _description_
+        :type deleting_file: str
+        :raises OSError: An error is raisen if files cannot be removed (arbitrary reasons).
+        :raises Valueerror: If parsed string is empty.
+        """
         try:
             if deleting_file == "":
-                print("No files to remove")
+                raise ValueError("No files to remove")
             else:
                 os.remove(deleting_file)
                 print("File removed sucessfully")
@@ -110,7 +149,7 @@ class DeleteHandler():
 
 # syntacs for automatic appling of programm
 def main():
-    """ Function to execute codes below by executing complete script. Means if complete code-file gets started the lines in this function will be executed."""
+    """ Function to execute codes below by executing complete script. That means if complete code-file gets started the lines in this function will be executed."""
     # define path
     PATH = "./"
     # create instance
@@ -120,12 +159,3 @@ def main():
 
 if __name__ == "main":
     main()
-
-# test script
-# PATH = "/home/opti/Oli_develop_deleting-function/"
-# pattern = "40*[8]*"
-
-# create instance
-# delete_master = DeleteHandler(PATH, pattern)
-
-# delete_master.delete_by_amount(20)
