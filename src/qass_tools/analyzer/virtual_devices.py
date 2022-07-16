d57b056b87d75768d17a461c778c6f80f497a6ad
UEWiebelitz <ewiebelitz@qass.net>
2022-07-16 23:01:48 +0200

    virtual_devices.py: Prepare for Config persistence and GUI
    Extend the interface and fix bugs to enable persistent configurations and cumstom config dialogs.
    Changed the implementation of VirtualInputDevice: The complete config is now stored in a dictionary to simplify the communication with the GUI.
    
    The DeviceTypeCollection does not expect a dictionary of devices anymore but a List of devices.
    The mapping from the name strings to the devices is done afterwards.
    The devices' names must be unique!

--- src/qass_tools/analyzer/virtual_devices.py
+++ src/qass_tools/analyzer/virtual_devices.py
@@ -14,31 +14,38 @@ from Analyzer.Devices import VirtDeviceInterface
 from Analyzer.Core import Log_IF
 
 # to not block the Analyzer4D software when reading data each virtual device works in its own thread.
-from PySide2.QtCore import QThread
+from PySide2.QtCore import QThread, QByteArray, Slot
+from PySide2.QtWidgets import QDialog
 from threading import Lock
 
 from abc import ABC, abstractmethod
 import traceback
 from typing import List, Dict
+import json
 
 class VirtualInputDevice(ABC):
     """
     VirtualInputDevice is a simplified interface for custom virtual devices in the Analyzer4D software.
     The methods marked with "abstractmethod" must be implemented.
     The other methods can be overridden if needed.
-    
+
     This class is meant to be used by DeviceHandler (see below).
     The device handler implements the actual Analyzer4D interface class while using instances of this class for the communication.
     """
 
-    def __init__(self, 
-                 sample_rate: float, 
-                 normal_amp: float, 
-                 request_rate: float=100, 
+    def __init__(self,
+                 name: str,
+                 sample_rate: float,
+                 normal_amp: float,
+                 request_rate: float=100,
                  dont_close: bool=False):
         """instantiate a SimpleDevice.
-        This class is an abstract base class. 
+        This class is an abstract base class.
         You must to derive from this class and implement the abstract funtions to create a custom device.
+
+        :param name: This is the name of the device that will also be displayed in the GUI.
+        The name is used as an identifier and thus it must be unique inside one DeviceTypeCollection.
+        :type name: str
 
         :param sample_rate: This is the sample rate that is used by the Analyzer4D software to build the Data signal stream.
         All values are assumed to have the same distance in time.
@@ -58,22 +65,26 @@ class VirtualInputDevice(ABC):
         If False the method open_connection() will be called at the beginning of each measurement and close_connection() will be called at the end of each measurement.
         Defaults to False.
         :type dont_close: bool, optional
-        """        
-           
-        self.__sample_rate = sample_rate
-        self.__normal_amp = normal_amp
-        self.__request_rate = request_rate
-        self.dont_close = dont_close
-    
+        """
+
+        self._config = {
+            'sample_rate': sample_rate,
+            'normal_amp': normal_amp,
+            'request_rate': request_rate,
+            'dont_close': dont_close
+            }
+
+        self._name = name
+
     @abstractmethod
     def is_available(self) -> bool:
         """is_available should indicate whether its possible to establish a connection to the device.
 
-        :return: True if the device is available and ready for measurements, False otherwise. 
+        :return: True if the device is available and ready for measurements, False otherwise.
         :rtype: bool
         """
         ...
-    
+
     @abstractmethod
     def open_connection(self):
         """open_connection() will be called before a measurement.
@@ -83,7 +94,7 @@ class VirtualInputDevice(ABC):
         raise an exception if needed. It will be handled by the wrapping class DeviceHandler.
         """
         ...
-    
+
     @abstractmethod
     def close_connection(self):
         """close_connection() will be called to close the connection to the device (usually at the end of a measurement).
@@ -93,23 +104,23 @@ class VirtualInputDevice(ABC):
         raise an exception if needed. It will be handled by the wrapping class DeviceHandler.
         """
         ...
-        
+
     @abstractmethod
     def get_data(self) -> List[float]:
         """Fetch the data from the device and convert it to a List of floats.
         Note: Ensure that the data type is float and not np.float or anything else!
         This method should always clear the buffer containing the data when it's called.
         This is to prevent old data from persisting.
-        
+
         :return: The list of new values. The list must not contain old values!
         :rtype: List[float]
         """
         ...
-    
+
     # overide the functions below if needed
     def init_device(self):
         return None
-    
+
     def config_dialog(self):
         """If this function returns a QDialog this will be displayed in the Analyzer4D software to setup the device.
         The settings button will only be available in the software if at least one device provides a QDialog and not None in this funciton.
@@ -118,18 +129,42 @@ class VirtualInputDevice(ABC):
         :rtype: _type_
         """
         return None
-    
+
     @property
     def requests_per_sec(self):
-        return self.__request_rate
-    
+        return self._config['request_rate']
+
     @property
     def sample_rate(self):
-        return self.__sample_rate
-    
+        return self._config['sample_rate']
+
     @property
     def normal_amplitude(self):
-        return self.__normal_amp
+        return self._config['normal_amp']
+
+    @property
+    def dont_close(self):
+        return self._config['dont_close']
+
+    @property
+    def dont_close(self):
+        return self._config['dont_close']
+
+    @property
+    def name(self):
+        return self._name
+
+    def get_config(self):
+        print('called get config')
+        return self._config
+
+    def set_config(self, config):
+        print('called set config', config)
+        self._config = config
+
+    def apply_config(self):
+        print('called apply config')
+        pass
 
 
 class _DeviceThread(QThread):
@@ -144,7 +179,7 @@ class _DeviceThread(QThread):
         self.values = []
         self.lock = Lock()
         self.device = device
-    
+
     def fetch_values(self) -> List[float]:
         """fetch_values is called by the interface from the reading thread in the Analyzer4D software.
         It first aqcuires the lock to prevent race conditions when fetching the values.
@@ -155,12 +190,12 @@ class _DeviceThread(QThread):
         with self.lock:
             vals = self.values
             self.values = []
-        
+
         return vals
-    
+
     def stop(self):
         self.should_stop = True
-    
+
     def run(self):
         """The run method is implicitly called by the QThread class.
         It is executed in a seperate thread.
@@ -173,15 +208,15 @@ class _DeviceThread(QThread):
             self.should_stop = False
             with self.lock:
                 self.values = []
-            
+
             if not self.device.dont_close:
                 self.device.open_connection()
-            
+
             # empty the data queue at start - we do not want to use old data
             self.device.get_data()
-            
+
             request_rate = self.device.requests_per_sec
-            
+
             while not self.should_stop:
                 new_data = self.device.get_data()
                 with self.lock:
@@ -191,7 +226,7 @@ class _DeviceThread(QThread):
                     QThread.usleep(1/request_rate * 1e6)
                 else:
                     self.yieldCurrentThread()
-            
+
             if not self.device.dont_close:
                 self.device.close_connection()
         except Exception:
@@ -206,37 +241,41 @@ class DeviceTypeCollection(VirtDeviceInterface):
     A DeviceClass is a class of hardware devices.
     Usually all devices handled by this class work similar to each other and support the same features.
     """
-    def __init__(self, devices: Dict[str, VirtualInputDevice], name:str='', version:str=''):
+    def __init__(self, devices: List[VirtualInputDevice], name:str='', version:str=''):
         """
         The constructor expects a .
         The devices are expected to be of the type VirtualInputDevice.
 
-        :param devices: dictionary giving the mappping from the device's identifiers to the device objects themselves
-        :type devices: Dict[str, VirtualInputDevice]
+        :param devices: List of the device. Their names are used as identifiers and have to be unique inside this DeviceTypeCollection.
+        :type devices: List[VirtualInputDevice]
         :param name: The name of this device class, defaults to ''
         :type name: str, optional
         :param version: The version string of this device class implementation, defaults to ''
         :type version: str, optional
         """
         super().__init__()
-        
-        self._devices = devices
+
         self._name = name
         self._version = version
-        
+
+        self._devices = {}
+        for dev in devices:
+            if dev.name in self._devices:
+                raise ValueError('The devices` names have to be unique, but got multiple devices with the same name')
+            self._devices[dev.name] = dev
+
         self._device_threads = {}
-        
         for name, dev in self._devices.items():
             self._device_threads[name] = _DeviceThread(dev)
-    
+
     def init(self):
         """
         This function is called by the Analyzer4D software.
         Usually device detection takes part here.
-        We assume in this implementatoin that available devices have been passed in the constructor.  
+        We assume in this implementatoin that available devices have been passed in the constructor.
         """
         pass
-    
+
     def capabilities(self) -> Dict:
         """capabilites is called by the Analyzer4D software to get information about available devices and their configuration.
         Especially the sample rate and the capability of configuration dialogs is queried here.
@@ -245,29 +284,29 @@ class DeviceTypeCollection(VirtDeviceInterface):
         The 'configdialog' is a boolean indicating whether a QDialog will be displayed
         Missing keys in the config will
 
-        :return: A dictionary containing information about this device class and the devices themselves. 
+        :return: A dictionary containing information about this device class and the devices themselves.
         :rtype: Dict
         """
         interfaces_conf = []
-        
+
         has_config = False
         for name, dev in self._devices.items():
             if dev.config_dialog() is not None:
                 has_config = True
-            
+
             int_conf = {
                 'ifaceName': name,
                 'sampleRate': dev.sample_rate,
                 'normalAmplitude': dev.normal_amplitude
             }
             interfaces_conf.append(int_conf)
-        
+
         config = {
             'inputs': interfaces_conf,
             'configdialog': has_config
         }
         return config
-    
+
     def versionString(self) -> str:
         """versionString
 
@@ -275,15 +314,15 @@ class DeviceTypeCollection(VirtDeviceInterface):
         :rtype: str
         """
         return self._version
-    
+
     def name(self) -> str:
         """name
-        
+
         :return: This device class' name.
         :rtype: str
         """
         return self._name
-    
+
     def isInputAvailable(self, name: str) -> bool:
         """isInputAvailable requests to check whether the given device is available and ready for measurements.
 
@@ -294,12 +333,12 @@ class DeviceTypeCollection(VirtDeviceInterface):
         """
         if name not in self._devices:
             return False
-        
+
         return self._devices[name].is_available()
-    
+
     def inputCount(self) -> int:
         return len(self._devices)
-    
+
     def openInput(self, name: str) -> bool:
         """openInput starts the communication and the data fetching for the given device.
         This function starts the device's thread.
@@ -313,9 +352,10 @@ class DeviceTypeCollection(VirtDeviceInterface):
         try:
             self._device_threads[name].start()
             return True
-        except Exception:
+        except Exception as e:
+            Log_IF.popupError(f'Exception caught during opening virtual device {name}:\n{traceback.format_exc()}')
             return False
-    
+
     def closeInput(self, name: str, prepareRestart = False) -> bool:
         """closeInput requests to stop the data collection for the given device.
         In this implementation this causes the device's thread to terminate.
@@ -330,9 +370,10 @@ class DeviceTypeCollection(VirtDeviceInterface):
         try:
             self._device_threads[name].stop()
             return True
-        except Exception:
+        except Exception as e:
+            Log_IF.popupError(f'Exception caught during closing virtual device {name}:\n{traceback.format_exc()}')
             return False
-    
+
     def readInput(self, name: str, stream: int=0) -> List[float]:
         """readInput fetches and returns the currently available data from the given device's thread.
 
@@ -344,3 +385,84 @@ class DeviceTypeCollection(VirtDeviceInterface):
         :rtype: List[float]
         """
         return self._device_threads[name].fetch_values()
+
+    def getConfig(self, name: str) -> QByteArray:
+        """The Analyzer4D software calls this function to make settings persistent in the database.
+        This implementation converts a dictionary based configuration from the device to the QByteArray that is expected by the Analyzer4D software.
+
+        :param name: The device's identifier string
+        :type name: str
+        :return: The byte array - the configuration's textual representation
+        :rtype: QByteArray
+        """
+        try:
+            config_dict = self._devices[name].get_config()
+            json_str = json.dumps(config_dict)
+            return json_str.encode()
+        except Exception as e:
+            Log_IF.popupError(f'Exception caught during getting config from virtual device {name}:\n{traceback.format_exc()}')
+
+    def setConfig(self, name: str, data: QByteArray) -> bool:
+        """setConfig is called after the device initialization to propagate saved settings from the database to the devices themselves.
+        This function translates the settings from a QByteArray representation to a json dictionary.
+
+        :param name: The device's identifier string
+        :type name: str
+        :param data: The byte array containing the textual representation of the device.
+        :type data: QByteArray
+        :return: True on success, False otherwise (if an exception has been raised)
+        :rtype: bool
+        """
+        try:
+            json_str = bytes(data).decode()
+            if json_str:
+                config_dict = json.loads(json_str)
+                self._devices[name].set_config(config_dict)
+            return True
+        except Exception as e:
+            Log_IF.popupError(f'Exception caught during setting config to virtual device {name}:\n{traceback.format_exc()}')
+            return False
+
+    def applyConfig(self, name: str) -> bool:
+        """applyConfig is called by the Analyzer4D software to request the device to apply the config.
+        This might include to communicate settings to a device.
+
+        :param name: The device's identifier string
+        :type name: str
+        :return: True on success, False otherwise (if an exception has been raised)
+        :rtype: bool
+        """
+        try:
+            self._devices[name].apply_config()
+            return True
+        except Exception as e:
+            Log_IF.popupError(f'Exception caught during config application of virtual device {name}:\n{traceback.format_exc()}')
+            return False
+
+    def createPluginDialog(self) -> QDialog:
+        """createPluginDialog allows to implement a custom dialog for this device type.
+        This implementation does not use this feature.
+        But you can override this function by subclassing this class...
+
+        :return: A QDialog instance.
+        :rtype: QDialog
+        """
+        return None
+
+    def createConfigDialog(self, name: str) -> QDialog:
+        """createConfigDialog is called by the Analyzer4D software to allow the user to configurate the device.
+        This mitght include file paths, IP addresses, Ports, sample rates or other device specific parameters.
+        If the device implementation supports this feature it must return a QDialog instance in its implementation of config_dialog().
+        This dialog is just forwarded here.
+
+        :param name: The device's identifier string
+        :type name: str
+        :return: The configuration QDialog instance.
+        :rtype: QDialog
+        """
+        print('called createConfigDialog')
+        try:
+            return self._devices[name].config_dialog()
+        except Exception as e:
+            Log_IF.popupError(f'Exception caught during creation of config dialog for virtual device {name}:\n{traceback.format_exc()}')
+            return None
