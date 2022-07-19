503e2a1ec30d7972cada96142638979830bd01cc
Nicklas Osmers <nosmers@qass.net>
2022-07-19 07:19:02 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- src/qass_tools/analyzer/deleting_process.py
+++ src/qass_tools/analyzer/deleting_process.py
@@ -85,9 +85,6 @@ class DeleteHandler():
         # read out currently used disk usage
         disk_usage = shutil.disk_usage(self.path)
         # help to debug: disk_usage[0] = total // disk_usage[1] = used // disk_usage[2] = free
-        #logging.debug("total", disk_usage[0])
-        #logging.debug("used", disk_usage[1])
-        #logging.debug("free", disk_usage[2])
         # calc free space that is required based on user limit
         target_free_space = disk_usage[0] * (1 - disk_usage_limit)
         # calc therefore required space
