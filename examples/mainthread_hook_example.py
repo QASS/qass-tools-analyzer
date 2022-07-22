15700157872eb64a3732e8a278d411833db7ce9f
UEWiebelitz <ewiebelitz@qass.net>
2022-07-23 00:14:54 +0200

    Add license information (LGPL V3).

--- examples/mainthread_hook_example.py
+++ examples/mainthread_hook_example.py
@@ -1,3 +1,22 @@
+#
+# Copyright (c) 2022 QASS GmbH.
+# Website: https://qass.net
+# Contact: QASS GmbH <info@qass.net>
+#
+# This file is part of Qass tools 
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, version 3 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 '''
 This is an example for the usage of the mainthread_hook.inMainThread decorator.
 This decorator can be used to execute a function in the main thread although the executing thread is not the main thread.
