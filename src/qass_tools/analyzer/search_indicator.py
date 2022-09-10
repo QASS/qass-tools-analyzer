5117b991b0ca1335be1b51b9ae9f098bd0c94f20
UEWiebelitz <ewiebelitz@qass.net>
2022-09-10 20:30:48 +0200

    search_indicator.py: Add a helper class to easily maintain a search progress bar in the OpenGL area of the Analyzer4D software.

---
+++ src/qass_tools/analyzer/search_indicator.py
@@ -0,0 +1,29 @@
+from Analyzer.OpNet import SearchInfo_IF, RunTimeInfo_IF
+from Analyzer.Core import Buffer_Py_IF
+
+
+class SearchIndicator:
+    def __init__(self, rti, out_connector, duration=1e6, label="analysis", ref_id=5, start_fband=None, end_fband=None) -> None:
+        self._stream = None
+        self._rti = rti
+        self._label = label
+        self._duration = duration
+        self._ref_id = ref_id
+        self._out_connector = out_connector
+        self._start_fband = start_fband
+        self._end_fband = end_fband
+ 
+    def process_start(self, stream: Buffer_Py_IF):
+        self._stream = stream
+        
+        self._si = SearchInfo_IF()
+        self._si.setSearchBuffer(self._stream)
+        self._si.setDuration(int(self._duration))
+        self._si.setLabel(self._label)
+        self._si.setRefID(int(self._ref_id))
+        self._si.setFStartHz(stream.getLowFrequency() if not self._start_fband else self._start_fband * stream.getFrqPerBand())
+        self._si.setFEndHz(stream.getHighFrequency() if not self._end_fband else self._end_fband * stream.getFrqPerBand())
+        
+    def tick(self):
+        self._si.setTrackingTime(self._rti.getCurrentTime())
+        self._out_connector.setOutputValue(self._si)
