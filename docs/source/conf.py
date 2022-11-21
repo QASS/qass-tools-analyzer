ddd5c44a23b35740697a20ece9ada475f5ee22f8
Nicklas Osmers <nosmers@qass.net>
2022-11-21 12:15:31 +0100

    remove mock import

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,8 +23,6 @@ extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.coverage'
 templates_path = ['_templates']
 exclude_patterns = []
 
-autodoc_mock_imports = ['qass']
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
