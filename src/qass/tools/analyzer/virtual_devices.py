f71f9a927552a02886bb6a2dd46b65471f4114ea
UEWiebelitz <ewiebelitz@qass.net>
2024-06-17 11:46:03 +0200

    virtual_devices.py: Fix import error with Qt 5.12
    Older Qt versions do not provide the class QDeadlineTimer.
    Removed the not needed import and replaced by int overload.

--- src/qass/tools/analyzer/virtual_devices.py
+++ src/qass/tools/analyzer/virtual_devices.py
@@ -33,7 +33,7 @@ from Analyzer.Devices import VirtDeviceInterface
 from Analyzer.Core import Log_IF
 
 # to not block the Analyzer4D software when reading data each virtual device works in its own thread.
-from PySide2.QtCore import QThread, QByteArray, Slot, QElapsedTimer, QDeadlineTimer
+from PySide2.QtCore import QThread, QByteArray, Slot, QElapsedTimer
 from PySide2.QtWidgets import QDialog
 from threading import Lock
 
